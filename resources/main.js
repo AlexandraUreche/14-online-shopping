20624bcfd525d8e464f2807171289c36a86b01ef
AlexandraUreche <70063621+AlexandraUreche@users.noreply.github.com>
2020-11-16 13:58:08 +0200

    changes after code review

--- resources/main.js
+++ resources/main.js
@@ -5,9 +5,7 @@ $(function () {
     const compositionProduct = $('.composition');
     const countryProduct = $('.country');
     const careInstuctProduct = $('.careInstructions-wrapper p');
-    const menuItems = $('header .menu-wrapper').find('a');
-    const pagesWrapper = $('div[class ^= "content"]');
-
+    const menuItems = $('header .menu-wrapper nav a');
     getproductHTML = function (index, productObj) {
         return `<div class="image-container">
         <div class="image-wrapper" data-index=${index} data-img=${productObj.imgUrl} data-id=${productObj.id} style="background-image: url(assets/${productObj.imgUrl})">
@@ -19,79 +17,56 @@ $(function () {
         `;
     };
 
+    function addProduct(currentProduct) {
+        for (let i = 0; i < products[currentProduct].length; i++) {
+            let productObj = products[currentProduct][i];
+            productHMTL = getproductHTML(i, productObj);
+            $('.gallery-wrapper').append(productHMTL);
+
+        }
+    }
+    addProduct(menuItems.data('product'));
+
     menuItems.click(function (e) {
-        if (!$(this).data('content')) {
+        if ($(this).data('product')) {
             e.preventDefault();
         }
-        // ce nu folosesti, e ok sa stergi, e bine sa fie cat mai curat codul
-        let viewPage = 0;
-
-        // este ok sa grupezi in functie de efectul dorit pe anumite sectiuni, mult mai curat si ursor de citit si urmarit
         menuItems.removeClass('selected');
         $(this).addClass('selected');
+        $('.gallery-wrapper').empty();
+        addProduct($(this).data('product'));
 
-        pagesWrapper.addClass('hidden');
-        let pageClass = $(this).attr('data-content');
-        $('.' + pageClass).removeClass('hidden');
-        let galleryWrapperPage = $('.' + pageClass + ' .gallery-wrapper');
-        
-        $('.categories-wrapper').removeClass('hidden');
-        if ($(this).data('count')) {
-            $(this).data('count', $(this).data('count') + 1);
-        }
-        else {
-            $(this).data('count', 1);
-        }
-        let currentProduct = $(this).data('product');
-        if ($(this).data('count') === 1) {
-            for (let i = 0; i < products[currentProduct].length; i++) {
-                let productObj = products[currentProduct][i];
-                productHMTL = getproductHTML(i, productObj);
-                galleryWrapperPage.append(productHMTL);
-
-            }
-        }
-        $('.gallery-wrapper').each(function () {
-            overlayFunct($(this));
-        });
-
-        function overlayFunct($currentGallery) {
-            $currentGallery.delegate('.image-wrapper', "click", function () {
-                let index = $(this).data('index');
-                // nu este good practice sa declari o variabila fara o valoare initiala
-                let prodIndex;
-                switch (currentProduct) {
-                    case 'coats':
-                        prodIndex = products.coats[index];
-                        break;
-                    case 'dresses':
-                        prodIndex = products.dresses[index];
-                        break;
-                    case 'jersey':
-                        prodIndex = products.jersey[index];
-                        break;
-                    case 'pants':
-                        prodIndex = products.pants[index];
-                        break;
-                        // poti sa folosesti default-ul pentru coats, de exemplu, nu este good practice sa nu pui nimic aici
-                    default:
-                        break;
-                }
-                $("#imgBig").css({ backgroundImage: "url(assets/" + $(this).data('img') + ")" })
-                titleProduct.text(prodIndex.name);
-                priceProduct.text(prodIndex.currency + prodIndex.price);
-                compositionProduct.text(prodIndex.composition);
-                countryProduct.text(prodIndex.country);
-                careInstuctProduct.text(prodIndex.care);
-                $("#overlay").show();
-
-            });
-        }
     });
+    $('.gallery-wrapper').delegate('.image-wrapper', "click", function () {
+        let index = $(this).data('index');
+        let prodIndex = products.coats[index];
+        switch ($('a').data('product')) {
+            case 'dresses':
+                prodIndex = products.dresses[index];
+                break;
+            case 'jersey':
+                prodIndex = products.jersey[index];
+                break;
+            case 'pants':
+                prodIndex = products.pants[index];
+                break;
+            default:
+                products.coats[index];
+                break;
+        }
+        $("#imgBig").css({ backgroundImage: "url(assets/" + $(this).data('img') + ")" })
+        titleProduct.text(prodIndex.name);
+        priceProduct.text(prodIndex.currency + prodIndex.price);
+        compositionProduct.text(prodIndex.composition);
+        countryProduct.text(prodIndex.country);
+        careInstuctProduct.text(prodIndex.care);
+        $("#overlay").show();
 
+    });
     const closeOverlay = $('.close-overlay-wrapper');
     closeOverlay.click(function () {
         $("#overlay").hide();
     });
+
 
 });
